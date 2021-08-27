800500d560ee4f114ac73e67ba94e106532b040e
Kateryna B <butsneva.ekaterina@gmail.com>
2021-08-26 19:49:43 -0500

    service-worker & indexed db set up

--- Develop/public/js/index.js
+++ Develop/public/js/index.js
@@ -135,7 +135,7 @@ function sendTransaction(isAdding) {
     }
   })
   .catch(err => {
-    // fetch failed, so save in indexed db
+    // if fetch fails, save in indexed db
     saveRecord(transaction);
 
     // clear form
